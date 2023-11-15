ddca2908c4bc9ffd68835fda8f42e711da4227e9
Tomek Krezymon <tomekkrezymon@gmail.com>
2023-11-16 00:27:32 +0100

    add products posting

--- api/main.py
+++ api/main.py
@@ -20,6 +20,24 @@ def remove_categories():
         if delete_response.status_code == 200 or delete_response.status_code == 404:
             print(f"Category {category_id} deleted successfully.")
 
+def remove_products():
+    import requests
+
+    # Get the list of categories
+    products_url = API_DEFAULT_LINK + 'products' + f'?ws_key={API_KEY}'
+    response = requests.get(products_url)
+    root = fromstring(response.content)
+
+    product_ids = [product.get('id') for product in root.findall('.//product')]
+    print(product_ids)
+    # Iterate through the category IDs and delete each one
+    for product_id in product_ids:
+        product_delete_url = API_DEFAULT_LINK + f'categories/{product_id}?ws_key={API_KEY}'
+        delete_response = requests.delete(product_delete_url)
+        print(delete_response.status_code)
+        if delete_response.status_code == 200 or delete_response.status_code == 404:
+            print(f"product {product_id} deleted successfully.")
+
 
     
 def add_category(category_name, parent_id=None):
@@ -29,24 +47,71 @@ def add_category(category_name, parent_id=None):
     category_schema["category"]["link_rewrite"]["language"]["value"] = category_name.lower().replace(" ", "-")
     category_schema["category"]["description"]["language"]["value"] = f'Produkty z kategorii {category_name}'
     return prestashop.add('categories', category_schema)["prestashop"]["category"]["id"]
-    
-#def process_products(products_data):
+def get_category_id(category_name):
+    return prestashop.get('categories', options={'filter[name]': category_name})["categories"]["category"]["attrs"]["id"]
 
+def convert_price_to_float(price):
+    tax = 1.23
+    return round(float(price.replace(",", ".").replace("zł", "").strip())/tax,2)
+
+def add_product(product):
+    
+    #DATA
+    product_schema["product"]["reference"] = product["id"]
+    product_schema["product"]["id_category_default"] = get_category_id(product["category"])
+    product_schema["product"]["name"]["language"]["value"] = product["name"]
+    product_schema["product"]["price"] = convert_price_to_float(product["price"])
+    print(product_schema["product"]["price"])
+    product_schema["product"]["link_rewrite"]["language"]["value"] = product["name"].lower().replace(" ", "-")
+    product_schema["product"]["meta_title"]["language"]["value"] = product["name"]
+    product_schema["product"]["weight"] = product["attributes"]["weight"]
+    product_schema["product"]["description_short"]["language"]["value"] = product["short_description"]
+    product_schema["product"]["description"]["language"]["value"] = f'{product["short_description"]}<br><strong>Masa:</strong> {product["attributes"]["weight"]}g<br><strong>Producent:</strong> {product["manufacturer"]}'
+
+    #OPTIONS
+    product_schema["product"]["id_tax_rules_group"] = 1
+    product_schema["product"]["active"] = 1
+    product_schema["product"]["state"] = 1
+    product_schema["product"]["available_for_order"] = 1
+    product_schema["product"]["minimal_quantity"] = 1
+    product_schema["product"]["show_price"] = 1
+    product_schema["product"]["id_shop_default"] = 1
+
+
+    prestashop.add("products", product_schema)["prestashop"]["product"]["id"]
 
 
 
 def process_categories(categories_data):
     #print categories and their subcategories
+    id = 2
     for main_category, subcategories in categories_data.items():
         print(main_category)
         parent_id = add_category(main_category, 1)
         for subcategory in subcategories:
             print(subcategory)
             add_category(subcategory, parent_id)
+        id += 1
         
 
         
-
+def process_products(products_data):
+    cnt = 0
+    for product in products_data:
+        if cnt > 1:
+            break
+        prod ={
+            "id": product["id"],
+            "price": product["price"],
+            "name": product["name"],
+            "short_description": product["short_description"],
+            "category": product["category"],
+            "attributes": product["attributes"],
+            "manufacturer": product["manufacturer"],
+        }
+        print("Adding product: ", prod["name"])
+        cnt += 1
+        add_product(prod)
 
 
 
@@ -65,10 +130,16 @@ if __name__ == "__main__":
         products_data = json.load(json_file)
 
     #remove_categories()
+    remove_products()
 
     category_schema = prestashop.get('categories', options={'schema': 'blank'})
+    product_schema = prestashop.get('products', options={'schema': 'blank'})
+    del product_schema["product"]["position_in_category"]
+    del product_schema["product"]["associations"]["combinations"]
 
-    process_categories(categories_data)
+
+    #process_categories(categories_data)
     #process_products(products_data)
 
-    
+    #linux command to zip prestashop and mariadb folders into prestashop.zip
+    #zip -r prestashop.zip prestashop mariadb
